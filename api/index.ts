cb4d237da32c1c389418a73e6f91c783df9e8100
kmabari <kmabarikiyafoods@gmail.com>
2026-07-30 15:39:00 +0000

    Test API endpoint

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,12 @@
-import app from "../server";
+import express from "express";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working"
+  });
+});
 
 export default app;
